5ed8f3ff888a7e272620c86e811ca1aab48819df
lilin <lilin@poissonsoft.com>
2023-04-25 14:37:13 +0800

    add normalise method.

---
+++ normalise.cpp
@@ -0,0 +1,4 @@
+		PK_VECTOR_t vector1 = { { 0.816497, 0.408248, 0.408248 } };
+		PK_VECTOR_t zeroVector = { {0.0, 0.0, 0.0} };
+		PK_VECTOR1_t zeroDir;
+		auto errCode = PK_VECTOR_normalise(zeroVector, &zeroDir);
